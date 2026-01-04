a7be3eb2a38ff9ebe6e0e0032caff470a3bc38a9
Vaughn <vaughnl2342@gmail.com>
2026-01-04 15:40:40 -0500

    Connects express bakcgournd-api pull, to React front end html page!! Adds CORS package to handle server request authentications

--- client/front_end/src/App.jsx
+++ client/front_end/src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const App = () => {
+
+    const [text, getData] = useState(null); //needed for react, cannot use document.get... this stores those variable and useEffect sets the values (React syntax)
+
+    useEffect(() => {
+    fetch("http://localhost:3000/testpage")
+        .then(res => res.text())
+        .then(result => getData(result));
+    }, []);
+
+    console.log(text);
+
   return (
+    //javascript goes here, above the html 
     <>
     <div>App</div>
     <h1>is this working? lol</h1>
+    <a href="http://localhost:3000/">Click Here for Express Backend!</a>
+    <h1 id='test'>{text}</h1>
     </>
   )
 }
