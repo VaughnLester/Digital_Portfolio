4f2f0ebdbe5a7a18a39cb616a4ae5ba6c4c98d50
Vaughn <vaughnl2342@gmail.com>
2026-01-11 15:58:11 -0500

    Properly pulls object data from express backend SQL database and inserts into html page with JSX objects, creating project 1 sample link and title!

--- client/front_end/src/App.jsx
+++ client/front_end/src/App.jsx
@@ -2,24 +2,22 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const App = () => {
-
-    const [text, getData] = useState(null); //needed for react, cannot use document.get... this stores those variable and useEffect sets the values (React syntax)
+    let x = [{"id":1,"link":"google.com"}];
+    const [text, getData] = useState([]); //needed for react, cannot use document.get... this stores those variable and useEffect sets the values (React syntax)
 
     useEffect(() => {
     fetch("http://localhost:3000/")
-        .then(res => res.text())
+        .then(res => res.json())
         .then(result => getData(result));
     }, []);
-
-    console.log(text);
 
   return (
     //javascript goes here, above the html 
     <>
     <div>App</div>
     <h1>is this working? lol</h1>
-    <a href="http://localhost:3000/">Click Here for Express Backend!</a>
-    <h1 id='test'>{text}</h1>
+    <a href="http://localhost:3000/">Click Here for Express Backend projects API!</a><br></br>
+    <a href={text[0] && text[0].link}>Click here for link from Express backend project 1!</a>
     </>
   )
 }
