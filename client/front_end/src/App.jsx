d616339916b2736c18841a3c58cd5c09d215c219
Vaughn <vaughnl2342@gmail.com>
2026-01-12 07:31:03 -0500

    Adds D2G project and starts frontend design

--- client/front_end/src/App.jsx
+++ client/front_end/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const App = () => {
-    let x = [{"id":1,"link":"google.com"}];
-    const [text, getData] = useState([]); //needed for react, cannot use document.get... this stores those variable and useEffect sets the values (React syntax)
+    const [projects, getData] = useState([]); //needed for react, cannot use document.get... this stores those variable and useEffect sets the values (React syntax)
 
     useEffect(() => {
-    fetch("http://localhost:3000/")
+    fetch("http://localhost:3000/projectAPI")
         .then(res => res.json())
         .then(result => getData(result));
     }, []);
@@ -14,10 +13,9 @@ const App = () => {
   return (
     //javascript goes here, above the html 
     <>
-    <div>App</div>
-    <h1>is this working? lol</h1>
-    <a href="http://localhost:3000/">Click Here for Express Backend projects API!</a><br></br>
-    <a href={text[0] && text[0].link}>Click here for link from Express backend project 1!</a>
+    <div></div>
+    <h1>Vaughn Lester: Digital Portfolio</h1><br></br>
+    <a href={projects[0] && projects[0].link}>D2G Digital Documentation Dump</a>
     </>
   )
 }
