7a73147276841f81d16786ba8554140f95a91c8e
Vaughn <vaughnl2342@gmail.com>
2026-03-07 14:38:31 -0500

    Makes slight improvmenets to footer and about content. Stage 1 ready for deployment!

--- client/front_end/src/components/footer.jsx
+++ client/front_end/src/components/footer.jsx
@@ -9,7 +9,7 @@ function Footer(){
             <h1 className="footerChild">Vaughn Lester</h1>
             <h1 className="footerChild">vaughnl2342@gmail.com</h1>
             <h1 className="footerChild">774-826-9906</h1>
-            <text className="footerChild">LinkedIn</text>
+            <h1 className="footerChild"><a href="https:www.linkedin.com/in/vaughn-lester/">LinkedIn</a></h1>
         </div>
     );
 }
--- client/front_end/src/components/page_content.jsx
+++ client/front_end/src/components/page_content.jsx
@@ -19,10 +19,7 @@ function Page_content(){
         <div className="pageContent">
             <div className="project1">
                 <h1>Project 1:</h1>
-                <a href={projects[0] && projects[0].link}> <h1>Vaughn Lester: D2G Digital Documentation Dump</h1> </a>
-            </div>
-            <div className="project2">
-                <h1>This is project 2</h1>
+                <a href={projects[0] && projects[0].link}> <h1>Displays2Go: D2G Digital Documentation Dump</h1> </a>
             </div>
         </div>
     );
