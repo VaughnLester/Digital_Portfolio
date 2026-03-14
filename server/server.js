73ac97f4c96204ee33a6a27620ae9129f8bb02bc
Vaughn <vaughnl2342@gmail.com>
2026-03-14 17:38:47 -0400

    Updates express server to properly run the react frontend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-app = express();
-const router = express.Router();
 const path = require("path");
+const fs = require("fs");
 
-//Server 
-port = 5000;
-app.listen(port, ()=>{console.log(`Server listening on port ${port}`)});
+const app = express();
+const port =  5000; // Use env PORT if provided
 
-//Static server
-app.use(express.static(path.join((__dirname) ,"static_server")));
+// Path to React build folder
+const ReactBuildPath = path.join(__dirname, "../client/front_end/dist");
+const indexPath = path.join(ReactBuildPath, "index.html");
+
+// Serve all static files (JS, CSS, images) from React build
+app.use(express.static(ReactBuildPath));
+
+// Catch-all route: serve React index.html for all other paths
+app.get("*", (req, res) => {
+  res.sendFile(indexPath);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
